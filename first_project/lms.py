0eb97449a4ead37c59ca0fd2cf9c43851103a150
Codigo Aldea <dmdndrd@gmail.com>
2026-02-23 13:44:33 +0530

    project commit

---
+++ first_project/lms.py
@@ -0,0 +1,8 @@
+"""
+this is the first project for the LMS with python
+"""
+
+
+# input from the admin 
+
+admin_choice = input(" Enter 1 for book issue, Enter 2 for add book, Enter 3 for delete book, Enter 4 for add user, Enter 5 for delete user ")
